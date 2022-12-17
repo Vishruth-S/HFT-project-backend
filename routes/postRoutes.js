97768d4b964e10440565046c876697605e069532
NzM <nazimfilzer@gmail.com>
2022-12-17 16:11:06 +0530

    Testing Image

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const { protectUser } = require('../middlewares/auth')
 const { createPost } = require('../controllers/post/createPost')
 const {acceptReq}=require('../controllers/post/acceptReq')
 const { getPost } = require("../controllers/post/getPost")
 
+const { uploadImage } = require('../controllers/post/uploadImage')
+const { getImage } = require('../controllers/post/getImage')
 
 
 // BASE URL - /api/post
@@ -14,6 +19,9 @@ const { getPost } = require("../controllers/post/getPost")
 router.get('/',protectUser, getPost)
 router.post('/', protectUser, createPost)
 router.put('/acceptReq/:id', protectUser, acceptReq);
+
+router.put('/image/:id', upload.single('file'), protectUser, uploadImage)
+router.get('/image', protectUser, getImage)
 
 
 module.exports = router;
